97bf8b79b9babfc71d0ef105459c86bc83ed9918
dariader <dary.spb@gmail.com>
2020-02-26 16:43:29 +0300

    draft fasta parser. Output - csv.

--- fasta_parser.sh
+++ fasta_parser.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
-file="/media/daria_der/linuxdata/neisseria_virulence/W_sg/BIGSdb_028077_1580937472_69979.xmfa"
-``` Скрипт чтобы парсить фасту в таблицу.  Очень тяжелые файлы на выходе. Надо делать предобработку.
+#file="/media/daria_der/linuxdata/neisseria_virulence/W_sg/BIGSdb_028077_1580937472_69979.xmfa"
+#xmfa="/media/daria_der/linuxdata/neisseria_virulence/pen_res/BIGSdb_014516_1582581087_34991_core.xmfa"
+xmfa="/media/daria_der/linuxdata/neisseria_virulence/pen_res/abres_core.xmfa"
+sed 's/-//g' $xmfa| sed 's/ + /___/g' > "$xmfa"_edited
+/home/daria_der/Downloads/Soft/faTrans "$xmfa"_edited "$xmfa"_translated
+file="$xmfa"_translated
+
+
+```
+Скрипт чтобы парсить фасту в таблицу.  Очень тяжелые файлы на выходе. Надо делать предобработку.
 ```
 #linearize fasta
 
+expected_number_of_samples="647"
+
 linearized_fasta=$(cat $file|sed '/^>/ s/$/;/'|sed ':a;N;$!ba;s/\n//g' | sed 's/>/\n>/g'| sed 's/+/_/g'| sed 's/ //g')
-proteins=$(cat $file| grep ">"|awk '{print $3}'|sort -u) ## list of proteins to grep
+#proteins=$(cat $file| grep ">"|awk '{print $3}'|sort -u) ## list of proteins to grep
+proteins=$(cat $file| grep ">"|awk -F'___' '{print $2}'| sort -u)
 	for i in $proteins; do
 	echo $i;
-	prep_fasta=$(printf '%q\n' $linearized_fasta|grep $i| sed 's/;/\n/g'|grep -v ">");
+	fasta_to_align=$(printf '%q\n' $linearized_fasta|grep $i| sed 's/;/\n/g')
+	prep_fasta=$(printf '%q\n' $fasta_to_align|sed 's/\\//g'| mafft - |sed '/^>/ s/$/;/'|sed ':a;N;$!ba;s/\n//g' | sed 's/>/\n>/g'| sed 's/+/_/g'| sed 's/ //g'| sed 's/;/\n/g'| grep -v ">");
+	num_of_samples=$(printf '%q\n' $prep_fasta| wc -l)
+	echo $num_of_samples
+	[ "$num_of_samples" -ne "$expected_number_of_samples" ] && echo "num of samples is weird" && continue || echo "num samples is OK"
 	length_fasta=$(printf '%q\n' $prep_fasta|awk '{ print length }'| head -1)
 	echo $length_fasta;
 	prep_fasta_fin=$(printf '%q\n' $prep_fasta|sed 's/=//g'|sed 's/>u.*\|/,/g'| sed 's/^,//g'); # fasta with commas
 	header=$(for (( j=1; j < ${length_fasta}; j++ )); do echo $j"."$i; done| sed ':a;N;$!ba;s/\n/,/g');
 	printf '%s\n' "$header" "$prep_fasta_fin"> "$i"_parsed.fasta;
       done
-
 paste *_parsed.fasta| sed 's/\t1./,1./g'|sed 's/,\t/,/g'| sed 's/,,/,/g'| sed 's/,$//g' > concatenated_fasta.csv
+rm *_parsed.fasta
--- table_parser.sh
+++ table_parser.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-data="/media/daria_der/linuxdata/neisseria_virulence/W_sg/concatenated_fasta.csv"
+#data="/media/daria_der/linuxdata/neisseria_virulence/W_sg/concatenated_fasta.csv"
+data="concatenated_fasta.csv"
 touch "resulting_dataframe.csv"
 
 i=$(head -1 $data| sed 's/,/\n/g'| wc -l)
 
-for((n = 1; n<i+1; n++))
+for((n = 1; n<i; n++))
 #for((n = 3; n<10; n++))
  do
   echo "$n"
   column=$(awk -F',' -v var="$n" '{print $var}' $data| sed '1d') ## array without first row
   draft_levels=$(echo $column|sed 's/,/\n/g'|sort -u|sed 's/X//g'| sed 's/-//g') ## set of true aminoacids in array
   levels=$(printf '%s\n' $draft_levels| sort -u)
-  #echo $levels
+  echo $levels
   length_levels=$(printf '%s\n' $levels| wc -l) #number of different aminoacids
   to_past=$(awk -F',' -v var="$n" '{print $var}' $data)
   one=$([ "$length_levels" -gt 1 ] && echo "1" || echo "0") ## test if there is at least one different aminoacid
   #echo "one is $one"
   #echo "length_levels $length_levels"
   printf '%s\n' $to_past > to_past.csv
- /usr/bin/time [ "$one" -eq "1" ] && paste resulting_dataframe.csv to_past.csv > resulting_dataframe_2.csv && rm resulting_dataframe.csv && mv resulting_dataframe_2.csv resulting_dataframe.csv >> writing_time## append new column to res df
+  [ "$one" -eq "1" ] && paste resulting_dataframe.csv to_past.csv > resulting_dataframe_2.csv && rm resulting_dataframe.csv && mv resulting_dataframe_2.csv resulting_dataframe.csv ## append new column to res df
   done
 
